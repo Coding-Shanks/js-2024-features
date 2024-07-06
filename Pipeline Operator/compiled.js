98b64f93bb442843b765d445c37759295ec5eb3c
Abhishek <136569777+Coding-Shanks@users.noreply.github.com>
2024-07-06 15:41:28 +0530

    Add files via upload

---
+++ Pipeline Operator/compiled.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _ref, _;
+// index.js
+var add5 = function add5(x) {
+  return x + 5;
+};
+var multiply2 = function multiply2(x) {
+  return x * 2;
+};
+var result = (_ref = (_ = 10, add5(_)), multiply2(_ref));
+console.log(result); // Output: 30
---
+++ Pipeline Operator/index.js
@@ -0,0 +1,9 @@
+// index.js
+const add5 = x => x + 5;
+const multiply2 = x => x * 2;
+
+const result = 10
+    |> add5
+    |> multiply2;
+
+console.log(result); // Output: 30
