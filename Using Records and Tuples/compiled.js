a26fc65a651c29518928884357d4e0f0c6a232a0
Abhishek <136569777+Coding-Shanks@users.noreply.github.com>
2024-07-06 15:44:07 +0530

    Add files via upload

---
+++ Using Records and Tuples/compiled.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// index.js
+var record = {
+  name: 'Alice',
+  age: 30
+};
+var tuple = [10, 20, 30];
+console.log(record.name); // Output: Alice
+console.log(tuple[1]); // Output: 20
---
+++ Using Records and Tuples/index.js
@@ -0,0 +1,6 @@
+// index.js
+const record = { name: 'Alice', age: 30 };
+const tuple = [10, 20, 30];
+
+console.log(record.name); // Output: Alice
+console.log(tuple[1]); // Output: 20
